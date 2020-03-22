5d362efbdb9d202ae430109d276d58a8e2f6ecf6
italianot <radioctiveboi@mail.ru>
2020-03-22 20:50:54 +0800

    Отредактировал

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -114,7 +114,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(266, 177);
+            this.ClientSize = new System.Drawing.Size(266, 178);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtResult);
